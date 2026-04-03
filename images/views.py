4079b757e88f828b75b0c223c0f1ba369b6b586b
blackbox24 <nicardid@gmail.com>
2026-04-03 14:57:49 +0000

    feat: list and create image

--- images/views.py
+++ images/views.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
@@ -10,9 +11,10 @@ class ListCreateImageView(ListCreateAPIView):
     parser_classes = (MultiPartParser, FormParser)
     permission_classes = (IsAuthenticated,)
     serializer_class = ImageSerializer
+    queryset = Images.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    def get_queryset(self):
-        return Images.objects.filter(user=self.request.user.pk)
+    def get_queryset(self, *args, **kwargs) -> QuerySet[Images]:
+        return Images.objects.filter(user=self.request.user.pk).prefetch_related()
