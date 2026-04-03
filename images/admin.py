78ddae92c13d3678f77c7bcb3a0835dd30a27e06
blackbox24 <nicardid@gmail.com>
2026-04-03 13:25:20 +0000

    feat: list and upload images

--- images/admin.py
+++ images/admin.py
@@ -1,1 +1,9 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import Images
+
+
+@admin.register(Images)
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ["user", "image", "created_at"]
--- images/views.py
+++ images/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 
+from .models import Images
 from .serializers import ImageSerializer
 
 
@@ -12,3 +13,6 @@ class ListCreateImageView(ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Images.objects.filter(user=self.request.user.pk)
